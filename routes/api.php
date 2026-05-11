e4a94ac1f3b211c09404ef74032c7fa8785115bf
Nurul-Ilmi <60200124036@uin-alauddin.ac.id>
2026-05-11 18:20:40 +0800

    Menyelesaikan seluruh soal Laporan 2 Hotel

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// 1. Import Controller yang sudah dibuat
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ItemController;
 
+use App\Http\Controllers\RoomTypeController;
+use App\Http\Controllers\HotelRoomController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,7 +24,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// 2. Daftarkan Route API untuk Categories dan Items
-// apiResource akan otomatis membuat rute GET, POST, PUT, dan DELETE
+
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('items', ItemController::class);
+
+Route::apiResource('room-types', RoomTypeController::class);
+Route::apiResource('hotel-rooms', HotelRoomController::class);
